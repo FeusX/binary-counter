272bb1cb1edb61b73ccba7bc024322cac55ce007
FeusX <132133667+FeusX@users.noreply.github.com>
2025-05-23 15:57:34 +0300

    better code

--- binary_counter.ino
+++ binary_counter.ino
@@ -8,9 +8,9 @@ void setup() {
   pinMode(D7, OUTPUT);
   pinMode(D1, OUTPUT);
   pinMode(D5, OUTPUT);
-  pinMode(D4, OUTPUT);
+  pinMode(D2, OUTPUT);
   pinMode(D0, OUTPUT);
-  pinMode(D6, OUTPUT);
+  pinMode(D6, OUTPUT); // buzzer
 }
 
 void loop() {
@@ -19,12 +19,12 @@ void loop() {
   digitalWrite(D7, (countbinary & 0x02) ? HIGH : LOW);
   digitalWrite(D1, (countbinary & 0x04) ? HIGH : LOW);
   digitalWrite(D5, (countbinary & 0x08) ? HIGH : LOW);
-  digitalWrite(D4, (countbinary & 0x10) ? HIGH : LOW);
+  digitalWrite(D2, (countbinary & 0x10) ? HIGH : LOW);
   digitalWrite(D0, (countbinary & 0x20) ? HIGH : LOW);
 
   countbinary++;
   if (countbinary > 63) {countbinary = 0;}
-  tone(D6, 700, 50);
+  tone(D6, 2000, 50); // you can change frequency (hertz) by changing 2000 value
 
   delay(500);
 }
